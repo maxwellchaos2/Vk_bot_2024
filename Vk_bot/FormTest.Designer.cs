08e12068d4a30619a40960e51c10f0ab9eedfe0d
user <user@DESKTOP-6SBM5M9>
2024-06-13 13:14:34 +0300

    проба изменений

--- Vk_bot/FormTest.Designer.cs
+++ Vk_bot/FormTest.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "FormTest";
-            this.Text = "FormTest";
+            this.Text = "FormTest1";
             this.Load += new System.EventHandler(this.FormTest_Load);
             this.ResumeLayout(false);
 
